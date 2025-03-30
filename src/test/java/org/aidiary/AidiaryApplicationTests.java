5861f36c227ffb245c4936db869e21e2bffedc97
dongha <ssyy3034@naver.com>
2025-03-30 22:01:59 +0900

    first commit

---
+++ src/test/java/org/aidiary/AidiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aidiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
